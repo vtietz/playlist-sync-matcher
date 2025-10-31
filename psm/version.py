774e54cf842846bd5737689fa108574fb2540946
vtietz <gitlab@vintie.de>
2025-10-31 22:25:22 +0100

    chore: Bump version to 0.1.2-rc.2

--- psm/version.py
+++ psm/version.py
@@ -4,6 +4,6 @@ Update this file when cutting a new release tag. Keep semantic versioning.
 Automations (PyInstaller, CLI --version) import from here.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2-rc.2"
 
 __all__ = ["__version__"]
