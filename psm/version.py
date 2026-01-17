7fff7adbc3219355420e7bfb7bf2acb7f72d7fb1
vtietz <gitlab@vintie.de>
2026-01-17 17:04:50 +0100

    Bump version to 0.1.3

--- psm/version.py
+++ psm/version.py
@@ -4,6 +4,6 @@ Update this file when cutting a new release tag. Keep semantic versioning.
 Automations (PyInstaller, CLI --version) import from here.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ["__version__"]
